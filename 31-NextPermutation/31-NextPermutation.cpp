a3cbc97bdf6036c8bc17c9cbd1516792ea3b432f
manoj-1125 <kmmanoj.kag@gmail.com>
2026-07-20 00:02:19 +0530

    Create 31-NextPermutation

---
+++ 31-NextPermutation/31-NextPermutation.cpp
@@ -0,0 +1,25 @@
+// Last updated: 7/20/2026, 12:02:15 AM
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int index=-1;
+        int n=nums.size();
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                index=i;
+                break;
+            }
+        }
+        if(index==-1){
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+        for(int i=n-1;i>index;i--){
+            if(nums[i]>nums[index]){
+                swap(nums[index],nums[i]);
+                break;
+            }
+        }
+        reverse(nums.begin()+index+1,nums.end());
+    }
+};
