40dd53af1b73cbbf8f57c8c9834ec6f777ae69ad
manoj-1125 <kmmanoj.kag@gmail.com>
2026-07-20 00:01:06 +0530

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/125-ValidPalindrome.cpp
@@ -0,0 +1,24 @@
+// Last updated: 7/20/2026, 12:01:02 AM
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i = 0, j = s.size() - 1;
+        while (i < j) {
+            while (i < j) {
+
+                while (i < j && !isalnum(s[i]))
+                    i++;
+
+                while (i < j && !isalnum(s[j]))
+                    j--;
+
+                if (tolower(s[i]) != tolower(s[j]))
+                    return false;
+
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
+};
