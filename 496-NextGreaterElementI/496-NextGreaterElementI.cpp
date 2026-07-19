d109b4f1c5eccec758c9c356492a99be965d2e90
manoj-1125 <kmmanoj.kag@gmail.com>
2026-07-19 23:59:57 +0530

    Create 496-NextGreaterElementI

---
+++ 496-NextGreaterElementI/496-NextGreaterElementI.cpp
@@ -0,0 +1,25 @@
+// Last updated: 7/19/2026, 11:59:54 PM
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        unordered_map<int, int> map;
+        stack<int> st;
+        for (int i = nums2.size() - 1; i >= 0; i--) {
+            while (!st.empty() && st.top() <= nums2[i]) {
+                st.pop();
+            }
+            if (st.empty())
+                map[nums2[i]] = -1;
+            else
+                map[nums2[i]] = st.top();
+            st.push(nums2[i]);
+        }
+
+        vector<int> ans;
+        for (int x : nums1) {
+            ans.push_back(map[x]);
+        }
+        return ans;
+    }
+};
