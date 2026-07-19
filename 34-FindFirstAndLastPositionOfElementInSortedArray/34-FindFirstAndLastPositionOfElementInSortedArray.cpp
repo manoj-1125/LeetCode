b8704bfb87050e74127ebc3e1939869c6afd95ee
manoj-1125 <kmmanoj.kag@gmail.com>
2026-07-20 00:02:15 +0530

    Create 34-FindFirstAndLastPositionOfElementInSortedArray

---
+++ 34-FindFirstAndLastPositionOfElementInSortedArray/34-FindFirstAndLastPositionOfElementInSortedArray.cpp
@@ -0,0 +1,48 @@
+// Last updated: 7/20/2026, 12:02:11 AM
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        return {findFirst(nums, target), findLast(nums, target)};
+    }
+    int findFirst(vector<int>& nums, int target) {
+        int low = 0; 
+        int high = nums.size() - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                ans = mid;
+                high = mid - 1;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    int findLast(vector<int>& nums, int target) {
+        int low = 0; 
+        int high = nums.size() - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                ans = mid;
+                low  = mid + 1;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+};
