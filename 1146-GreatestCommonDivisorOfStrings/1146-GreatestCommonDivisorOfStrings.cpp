e5040445cedfe148b63ca516dad21ea0569c90dc
manoj-1125 <kmmanoj.kag@gmail.com>
2026-07-21 19:40:09 +0530

    Create 1146-GreatestCommonDivisorOfStrings

---
+++ 1146-GreatestCommonDivisorOfStrings/1146-GreatestCommonDivisorOfStrings.cpp
@@ -0,0 +1,10 @@
+// Last updated: 7/21/2026, 7:40:06 PM
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        if(str1+str2 != str2+str1)
+            return "";
+        int gcd_len=gcd(str1.size(),str2.size());
+        return str1.substr(0,gcd_len);
+    }
+};
