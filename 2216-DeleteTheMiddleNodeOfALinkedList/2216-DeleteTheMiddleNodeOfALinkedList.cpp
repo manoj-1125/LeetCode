97dac6ddbafc44588258d14d6c5637686e5e724d
Manoj K M <kmmanoj.kag@gmail.com>
2026-08-09 00:33:29 +0530

    Create 2216-DeleteTheMiddleNodeOfALinkedList

---
+++ 2216-DeleteTheMiddleNodeOfALinkedList/2216-DeleteTheMiddleNodeOfALinkedList.cpp
@@ -0,0 +1,26 @@
+// Last updated: 8/9/2026, 12:33:26 AM
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+        ListNode *slow=head, *fast=head;
+        if(head==nullptr || head->next==nullptr)
+            return NULL;
+        fast=fast->next->next;
+        while(fast!=nullptr && fast->next!=nullptr){
+            fast=fast->next->next;
+            slow=slow->next;
+        }
+        slow->next=slow->next->next;
+        return head;
+    }
+};
