864d13ddc041bdd27a2abc4cfab311cc3c57d685
Manoj K M <kmmanoj.kag@gmail.com>
2026-08-17 23:18:21 +0530

    Create 1390-AverageSellingPrice

---
+++ 1390-AverageSellingPrice/1390-AverageSellingPrice.sql
@@ -0,0 +1,9 @@
+-- Last updated: 8/17/2026, 11:18:18 PM
+# Write your MySQL query statement below
+select 
+    p.product_id, 
+    ROUND(COALESCE(SUM(p.price * u.units)/SUM(u.units),0),2) as average_price
+from prices p left join unitssold u
+on p.product_id=u.product_id
+and u.purchase_date between p.start_date and p.end_date
+group by p.product_id;
