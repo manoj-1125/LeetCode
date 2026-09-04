1e316fd169777bf49fac266d823f7aab89590cf4
Manoj K M <kmmanoj.kag@gmail.com>
2026-09-04 23:54:36 +0530

    Create 1934-ConfirmationRate

---
+++ 1934-ConfirmationRate/1934-ConfirmationRate.sql
@@ -0,0 +1,8 @@
+-- Last updated: 9/4/2026, 11:54:33 PM
+# Write your MySQL query statement below
+select 
+    s.user_id, 
+    ROUND(COALESCE(AVG(c.action="confirmed"),0),2) as confirmation_rate
+from signups s left join confirmations c
+on s.user_id=c.user_id
+group by s.user_id;
