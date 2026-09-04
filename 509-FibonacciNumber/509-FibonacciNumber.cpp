903a6c3db373e8c07fda118f7a2444b384c7cd8c
Manoj K M <kmmanoj.kag@gmail.com>
2026-09-04 23:56:23 +0530

    Create 509-FibonacciNumber

---
+++ 509-FibonacciNumber/509-FibonacciNumber.cpp
@@ -0,0 +1,15 @@
+// Last updated: 9/4/2026, 11:56:20 PM
+class Solution {
+public:
+    int fib(int n) {
+        if(n==0||n==1)
+            return n;
+        vector<int> dp(n+1);
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+};
