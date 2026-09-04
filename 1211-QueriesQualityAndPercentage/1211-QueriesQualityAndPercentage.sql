b335b1d3eed51e83b8834bba1abc7f301a67faec
Manoj K M <kmmanoj.kag@gmail.com>
2026-09-04 23:55:40 +0530

    Create 1211-QueriesQualityAndPercentage

---
+++ 1211-QueriesQualityAndPercentage/1211-QueriesQualityAndPercentage.sql
@@ -0,0 +1,7 @@
+-- Last updated: 9/4/2026, 11:55:37 PM
+SELECT
+    query_name,
+    ROUND(AVG(rating / position), 2) AS quality,
+    ROUND(AVG(CASE WHEN rating < 3 THEN 1 ELSE 0 END) * 100,2) AS poor_query_percentage
+FROM Queries
+GROUP BY query_name;
