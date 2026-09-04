fa7676757d7a180f14e84bad5bafe3f14d8bb9ad
Manoj K M <kmmanoj.kag@gmail.com>
2026-09-04 23:56:37 +0530

    Create 704-BinarySearch

---
+++ 704-BinarySearch/704-BinarySearch.cpp
@@ -0,0 +1,22 @@
+// Last updated: 9/4/2026, 11:56:34 PM
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+    int low=0;
+    int high=nums.size()-1;
+    while(low<=high){
+        int mid=low+(high-low)/2;
+
+        if(nums[mid]==target)
+            return mid;
+
+        else if(nums[mid]<target)
+            low=mid+1;
+
+        else
+            high=mid-1;
+    }
+
+    return -1;
+    }
+};
